6f9ba0e7d7ab9d454478024db8eaa66e3f645d5b
zubaryk <annazubaryk@tut.by>
2023-05-08 11:50:55 +0300

    feat: add decision 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,16 @@
+const fs = require('fs');
+let { COPYFILE_EXCL } = fs.constants;
 
+fs.mkdir('04-copy-directory/files-copy', err => {
+    if(err) throw err;
+    console.log('Папка успешно создана');
+    fs.readdir('04-copy-directory/files', (err, files) => {
+        files.forEach(file => {
+            fs.copyFile('04-copy-directory/files/'+file, '04-copy-directory/files-copy/'+file,  COPYFILE_EXCL, err => {
+                if (err) throw err;
+                console.log('Файлы успешно скопированы');
+            }  )
+        })
+    })
+
+});
