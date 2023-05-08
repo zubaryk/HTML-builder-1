f3caf9fb95172ccbe0e7067dd7c6bc5736a73959
zubaryk <annazubaryk@tut.by>
2023-05-08 10:27:44 +0300

    feat: add decision 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,8 +1,9 @@
+
 const path = require('path');
 const fs = require('fs');
 const folderPath = '03-files-in-folder/secret-folder';
 
-
+/*
         fs.stat(folderPath, (err, stats) => {
             fs.readdir(folderPath, (err, files) => {
                 files.forEach(file => {
@@ -18,12 +19,27 @@ const folderPath = '03-files-in-folder/secret-folder';
                 })
             })
         })
+*/
 
-  
-
-
-
-
+    fs.readdir('03-files-in-folder/secret-folder', (err, files) => {
+        files.forEach(file => {
+            fs.stat('03-files-in-folder/secret-folder/'+file, (err, stats) => {
+            if (err) {
+                 throw err
+            };
+            if(file){
+            stats.size;
+            path.parse(file).name;
+            path.extname(file);
+           console.log(`${path.parse(file).name} - ${path.extname(file)} - ${stats.size}`);
+            }
+        }
+        )
+    }
+        )
+}
     
 
+)
 
+    
